31351c2fed5ff5393548c3e8f91ea6909a7cab37
Viktor Kuroljov <viktor@ku.family>
2022-12-02 15:09:00 +0200

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: {
+      applyBaseStyles: false,
+    },
+  })]
 });

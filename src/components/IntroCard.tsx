98d43fc362488005ce8fec4b9a155648232ee2c8
Viktor Kuroljov <viktor@ku.family>
2022-12-03 10:40:11 +0200

    feat: make animations smoother in main page

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -2,12 +2,12 @@ import s from '../components/IntroCard.module.css'
 
 export default function Card({ name }: { name: string }) {
   return (
-    <a href={`/${name.toLowerCase()}`} class={s.card}>
-      <h1 class={`${s.title} text-white text-5xl font-extralight`}>{name}</h1>
+    <button class={s.card}>
+      <h1 class={s.title}>{name}</h1>
       <p class={s.more}>press arrow to see more</p>
       <svg class={s.arrow} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
         <path d="m18.294 16.793-5.293 5.293V1h-1v21.086l-5.295-5.294-.707.707L12.501 24l6.5-6.5-.707-.707z" />
       </svg>
-    </a>
+    </button>
   )
 }

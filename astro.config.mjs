aeca05fdab7bca4731405106818a07eca1023a0e
Viktor Kuroljov <viktor@ku.family>
2022-12-04 15:35:03 +0200

    feat: setup blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  integrations: [preact(), tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    config: {
-      applyBaseStyles: false,
-    },
-  })]
+  integrations: [
+    mdx(),
+    preact(),
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }),
+  ]
 });

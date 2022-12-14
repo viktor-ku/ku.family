b612991e8a6b6f1f4d3b6c3e58105cc13fac0f0f
Viktor Kuroljov <viktor@ku.family>
2022-12-15 01:59:42 +0200

    refactor: switch to react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [
-    mdx(),
-    preact(),
-    tailwind({
-      config: {
-        applyBaseStyles: false
-      }
-    }),
-  ]
+  integrations: [mdx(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react()]
 });
--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -9,18 +9,18 @@ export default function Card({ name, children, id }: { id: string, name: string,
   }
 
   return (
-    <button class={s.card} onClick={press}>
+    <button className={s.card} onClick={press}>
       <main>
-        <h1 class={`${s.title} ${id}`}>{name}</h1>
-        <h2 class={s.subtitle}>
+        <h1 className={`${s.title} ${id}`}>{name}</h1>
+        <h2 className={s.subtitle}>
           {children}
         </h2>
-        <svg class={s.arrow} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
+        <svg className={s.arrow} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
           <path d="m18.294 16.793-5.293 5.293V1h-1v21.086l-5.295-5.294-.707.707L12.501 24l6.5-6.5-.707-.707z" />
         </svg>
       </main>
       <footer>
-        <p class={s.hint}>press anywhere to continue</p>
+        <p className={s.hint}>press anywhere to continue</p>
       </footer>
     </button>
   )

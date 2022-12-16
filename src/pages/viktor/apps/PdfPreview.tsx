d1a810731380463ac2ea11a1149515b6aadb1ef3
Viktor Kuroljov <viktor@ku.family>
2022-12-16 07:27:21 +0200

    feat: extract invoice into PdfPreview

---
+++ src/pages/viktor/apps/PdfPreview.tsx
@@ -0,0 +1,9 @@
+import s from './PdfPreview.module.css'
+
+export default function PdfPreview() {
+  return (
+    <div className={s.sheet}>
+      <h1 className={`text-2xl text-center p-8 font-bold text-gray-300`}>Groiku OU</h1>
+    </div>
+  );
+}
